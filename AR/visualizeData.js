933936e886d6663dc1a0084fcbfffe310673ac85
PaulaScharf <paulascharf@gmx.de>
2019-11-27 20:54:52 +0100

    added gitignore and other minor fixes

--- AR/visualizeData.js
+++ AR/visualizeData.js
@@ -147,12 +147,71 @@ function selectData(dataArray){
 */
 function visualizeData(dataArray){
     JL("mylogger").info("--------visualizeData()--------");
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        const latitude = place.location.lat;
+    const longitude = place.location.lng;
+
+    // add place icon
+    const icon = document.createElement('a-image');
+    icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+    icon.setAttribute('name', place.name);
+    let imageSrc = "";
+    let expression = place.air_quality.pm10;
+    switch(true) {
+        case (expression < 5):
+            console.log("green");
+            imageSrc = './markers/map-marker_green.png';
+            break;
+        case (expression > 5 && expression < 6):
+            console.log("orange");
+            imageSrc = './markers/map-marker_orange.png';
+            break;
+        case (expression > 6):
+            console.log("red");
+            imageSrc = './markers/map-marker_red.png';
+            break;
+    }
+    icon.setAttribute('src', imageSrc);
+
+    // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+    icon.setAttribute('scale', '20, 20');
+
+    icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+
+    const clickListener = function (ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        const name = ev.target.getAttribute('name');
+
+        const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+        if (el && el === ev.target) {
+            const label = document.createElement('span');
+            const container = document.createElement('div');
+            container.setAttribute('id', 'place-label');
+            label.innerText = name;
+            container.appendChild(label);
+            document.body.appendChild(container);
+
+            setTimeout(() => {
+                container.parentElement.removeChild(container);
+        }, 1500);
+        }
+    };
+
+    icon.addEventListener('click', clickListener);
+
+    scene.appendChild(icon);
+});
 }
 
 
 function loadAndRenderMarkerLocations() {
     let places = LoadExamplePlaces();
-    renderPlaces(places);
+    visualizeData(places);
 }
 
 function LoadExamplePlaces() {
@@ -223,66 +282,4 @@ function LoadExamplePlaces() {
             }
         }
     ];
-}
-
-function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
-
-    places.forEach((place) => {
-        const latitude = place.location.lat;
-        const longitude = place.location.lng;
-
-        // add place icon
-        const icon = document.createElement('a-image');
-        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-        icon.setAttribute('name', place.name);
-        let imageSrc = "";
-        let expression = place.air_quality.pm10;
-        switch(true) {
-            case (expression < 5):
-                console.log("green");
-                imageSrc = './markers/map-marker_green.png';
-                break;
-            case (expression > 5 && expression < 6):
-                console.log("orange");
-                imageSrc = './markers/map-marker-orange.png';
-                break;
-            case (expression > 6):
-                console.log("red");
-                imageSrc = './markers/map-marker_red.png';
-                break;
-        }
-        icon.setAttribute('src', imageSrc);
-
-        // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-        icon.setAttribute('scale', '20, 20');
-
-        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-
-        const clickListener = function (ev) {
-            ev.stopPropagation();
-            ev.preventDefault();
-
-            const name = ev.target.getAttribute('name');
-
-            const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-            if (el && el === ev.target) {
-                const label = document.createElement('span');
-                const container = document.createElement('div');
-                container.setAttribute('id', 'place-label');
-                label.innerText = name;
-                container.appendChild(label);
-                document.body.appendChild(container);
-
-                setTimeout(() => {
-                    container.parentElement.removeChild(container);
-                }, 1500);
-            }
-        };
-
-        icon.addEventListener('click', clickListener);
-
-        scene.appendChild(icon);
-    });
 }
