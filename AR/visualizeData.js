def6e47438141fb04362f0421d161e379cc14928
Katharina Hovestadt <41635103+KathHv@users.noreply.github.com>
2019-11-27 14:50:48 +0100

    reading csv data

--- AR/visualizeData.js
+++ AR/visualizeData.js
@@ -8,54 +8,72 @@
 *@param currentPosition: current position of the device [lat,lng]
 *@param visArea: area in document where something can be visualized
 */
-var url = "/data/bike_14-12.csv";
+var url = "/data/bike_14-11.csv";
 var currentPosition;
 var visArea = document.getElementById("visArea");
+var loadedData=null;
+var selectedData=null;
+
+// "mylogger" logs to just the console
+//@see http://js.jsnlog.com/
+var consoleAppender = JL.createConsoleAppender('consoleAppender');
+JL("mylogger").setOptions({"appenders": [consoleAppender]});
 
 
 
-function processData(){
-  var loadedData = loadData();
-  JL("mylogger").warn("loadedData: " + loadedData);
-  var readData = selectData(loadedData);
-  JL("mylogger").warn("readData: " + readData);
-  var visualizedData = visualizeData(readData);
-  JL("mylogger").warn("visualizedData: " + visualizedData);
-
+function getLocation() {
+ if (navigator.geolocation) {
+   navigator.geolocation.getCurrentPosition(showPosition);
+ } else {
+   x.innerHTML = "Geolocation is not supported by this browser.";
+ }
 }
 
-	document.getElementById('button').onclick = function(e) {
-    var loadedData = loadData();
-    JL("mylogger").warn("loadedData: " + loadedData);
-    var readData = readData(loadedData);
-    JL("mylogger").warn("readData: " + readData);
-    var visualizedData = visualizeData(readData);
-    JL("mylogger").warn("visualizedData: " + visualizedData);
+function showPosition(position) {
+ x.innerHTML = "Latitude: " + position.coords.latitude +
+ "<br>Longitude: " + position.coords.longitude;
+}
 
-  }
+
+
+
+/*
+function processData(){
+  var loadedData=null;
+  var selectedData=null;
+
+  loadedData = loadData();
+  JL("mylogger").info("loadedData: " + loadedData);
+
+  selectedData = selectData(loadedData);
+  JL("mylogger").info("selectedData: " + selectedData);
+
+  //var visualizedData = visualizeData(readData);
+  //JL("mylogger").info("visualizedData: " + visualizedData);
+
+}*/
 
 /*
 * load Data from csv file
 *@return: array with air quality data in format: [[timestamp, record, lat, lon, AirTC_Avg, RH_Avg, pm25, pm10], ...]
 */
 function loadData(){
+
+  JL("mylogger").info("--------loadData()--------");
     var xhttp = new XMLHttpRequest();
 
-    xhttp.onreadystatechange = function() {
-
-      if (this.readyState == 4 && this.status == 200) {
+   xhttp.onreadystatechange = function() {
+       if (this.readyState == 4 && this.status == 200) {
         if (this.responseText.length == 0){
             JL("mylogger").error("The URL field or the content of the field is emtpy.1");
         }
-        JL("mylogger").info(this.responseText);
+        JL("mylogger").info("response Text: " + this.responseText);
         var dataArray = readData(this.responseText);
-        return dataArray;
+        loadedData = dataArray;
       }
     };
     xhttp.open("GET", url, true);
     xhttp.send();
-
-
 }
 
 
@@ -65,14 +83,19 @@ function loadData(){
 *@return returns array of processed csv-text in form [[timestamp, record, lat, lon, AirTC_Avg, RH_Avg, pm25, pm10], ...]
 */
 function readData(dataCSV){
-  var dataArraySplittedByBrake = dataCSV.split["\n"];
-  var dataArraySplittedByBrakeAndComma = null;
+  JL("mylogger").info("--------readData()--------");
+  var dataArraySplittedByBrake = dataCSV.split("\n");
+  dataArraySplittedByBrake.shift();
+//    JL("mylogger").info("dataArraySplittedByBrake[0]: " + dataArraySplittedByBrake[0]);
+  var dataArraySplittedByBrakeAndComma = [];
   var x;
   for (x in dataArraySplittedByBrake){
-    var dataElementSplittedByBrakeAndComma = dataArraySplittedByBrake[x].split[","];
+
+    var dataElementSplittedByBrakeAndComma = dataArraySplittedByBrake[x].split(",");
     dataElementSplittedByBrakeAndComma.shift();
     dataArraySplittedByBrakeAndComma.push(dataElementSplittedByBrakeAndComma);
   }
+  JL("mylogger").info("read data is ready.");
   return dataArraySplittedByBrakeAndComma
 }
 
@@ -83,6 +106,7 @@ function readData(dataCSV){
 *@return: array with relevant air quality data in format: [[timestamp, record, lat, lon, AirTC_Avg, RH_Avg, pm25, pm10], ...]
 */
 function selectData(dataArray){
+    JL("mylogger").info("--------selectData()--------");
 var relevantDataArray = null;
 var radius = 0.00001;
 
@@ -100,11 +124,16 @@ for (x in dataArray){
           || currentPosition[1] > (dataArray[x][3] - radius))
          )
     ){
-
+      JL("mylogger").info("relevant Position: " +dataArray[x][2]+", "+dataArray[x][3]);
       relevantDataArray.push(dataArray[x]);
     }
+    else{
+        JL("mylogger").info("position not relevant.");
+    }
 }
-return relevantDataArray;
+JL("mylogger").info("relevantDataArray: "+ relevantDataArray);
+selectedData =  relevantDataArray;
+
 }
 
 
@@ -113,4 +142,5 @@ return relevantDataArray;
 *@param dataArray: array which contains the RELEVANT data of the air quality in format [[timestamp, record, lat, lon, AirTC_Avg, RH_Avg, pm25, pm10], ...]
 */
 function visualizeData(dataArray){
+    JL("mylogger").info("--------visualizeData()--------");
 }
