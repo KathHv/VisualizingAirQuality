5d5f55442fcbbdd19668d4fb3f135f45c53bdb5b
paulascharf@gmx.de <paulascharf@gmx.de>
2019-12-02 13:02:43 +0100

    displaying locations as spheres

--- AR/visualizeData.js
+++ AR/visualizeData.js
@@ -149,10 +149,9 @@ function visualizeData(dataArray){
         let longitude = place.location.lng;
 
         // add place icon
-        let icon = document.createElement('a-plane');
+        let icon = document.createElement('a-sphere');
         icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        icon.setAttribute('src', '#ground');
-        icon.setAttribute('rotation', '-90 0 0');
+        icon.setAttribute('height', '128');
         icon.setAttribute('name', place.name);
         let color = getColor(place.air_quality.pm10);
         console.dir(color);
